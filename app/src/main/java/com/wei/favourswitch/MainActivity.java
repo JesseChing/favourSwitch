6c33511371f7a484c0248fc1bbf3b356505b7f52
覃威 <weiqin@cainfoharbor.com>
2017-10-31 18:03:24 +0800

    加上动画，动画的细节还需要进行调整

--- app/src/main/java/com/wei/favourswitch/MainActivity.java
+++ app/src/main/java/com/wei/favourswitch/MainActivity.java
@@ -4,11 +4,20 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
+    NumberSwitchView numberSwitchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        numberSwitchView = findViewById(R.id.view);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        numberSwitchView.startAnimation();
     }
 }
--- app/src/main/java/com/wei/favourswitch/NumberSwitchView.java
+++ app/src/main/java/com/wei/favourswitch/NumberSwitchView.java
@@ -3,13 +3,11 @@ package com.wei.favourswitch;
 import android.animation.Animator;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
-import android.animation.ValueAnimator;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
@@ -27,7 +25,7 @@ public class NumberSwitchView extends View {
     int oldNum;
     int newNum;
 
-    float ratio;
+    float ratio = 1f;
 
     AnimatorSet mAnimatorSet;
 
@@ -64,17 +62,19 @@ public class NumberSwitchView extends View {
         viewPaint.setTextSize(40);
         viewPaint.setColor(Color.RED);
 
-        Paint.FontMetrics fontMetrics = viewPaint.getFontMetrics();
-        float fontHeight = fontMetrics.bottom - fontMetrics.top;
+//        Paint.FontMetrics fontMetrics = viewPaint.getFontMetrics();
+//        float fontHeight = fontMetrics.bottom - fontMetrics.top;
+//
+//        Rect bounds = new Rect();
+//        String oldStr = String.valueOf(oldNum);
+//
+//        viewPaint.getTextBounds(oldStr, 0, oldStr.length(), bounds);
+//        canvas.drawText(oldStr, 0, fontHeight, viewPaint);
+//
+//        String newStr = String.valueOf(newNum);
+//        canvas.drawText(newStr, 0, fontHeight + viewPaint.getFontSpacing(), viewPaint);
 
-        Rect bounds = new Rect();
-        String oldStr = String.valueOf(oldNum);
-
-        viewPaint.getTextBounds(oldStr, 0, oldStr.length(), bounds);
-        canvas.drawText(oldStr, 0, fontHeight, viewPaint);
-
-        String newStr = String.valueOf(newNum);
-        canvas.drawText(newStr, 0, fontHeight + viewPaint.getFontSpacing(), viewPaint);
+        drawNum(canvas);
 
 
     }
@@ -86,10 +86,81 @@ public class NumberSwitchView extends View {
         diffPositionList = new ArrayList<>();
 
         oldNum = 123;
-        newNum = 124;
+        newNum = 21250;
     }
 
     private void compareStr(){
+
+    }
+
+    private void drawNum(Canvas canvas) {
+        String oldStr = String.valueOf(oldNum);
+        String newStr = String.valueOf(newNum);
+
+        viewPaint.setTextSize(40);
+        viewPaint.setColor(Color.RED);
+
+        Paint.FontMetrics fontMetrics = viewPaint.getFontMetrics();
+        float fontHeight = fontMetrics.bottom - fontMetrics.top;
+        float[] strWidths; //获取每个字符的宽度
+
+        short direction = 0;
+        int maxLength = 0;
+        if (oldNum > newNum) {
+            direction = -1;
+            maxLength = oldStr.length();
+            strWidths = new float[maxLength];
+            viewPaint.getTextWidths(oldStr, strWidths);
+        } else {
+            direction = 1;
+            maxLength = newStr.length();
+            strWidths = new float[maxLength];
+            viewPaint.getTextWidths(newStr, strWidths);
+        }
+
+
+//        RectF rect = new RectF(0,0,viewPaint.measureText(newStr),fontHeight);
+//        canvas.save();
+//        canvas.clipRect(rect);
+
+        float x_index = 0;
+        for (int i = 0; i < maxLength; i++) {
+            String subStr = "", subStr2 = "";
+            if (i < oldStr.length()) {
+                subStr = oldStr.substring(i, i + 1);
+            }
+
+            if (i < newStr.length()) {
+                subStr2 = newStr.substring(i, i + 1);
+            }
+
+            int cmpResult = subStr.compareTo(subStr2);
+            if (cmpResult > 0) {
+                direction = -1;
+            } else if (cmpResult < 0) {
+                direction = 1;
+            } else {
+                direction = 0;
+            }
+
+            if (!"".equals(subStr)) {
+                if (direction == 0) {
+                    canvas.drawText(subStr, x_index, 100 + fontHeight, viewPaint);
+                } else {
+                    canvas.drawText(subStr, x_index, 100 + fontHeight , viewPaint);
+                }
+            }
+
+            if (!"".equals(subStr2) && direction != 0) {
+                canvas.drawText(subStr2, x_index, 100 + fontHeight + viewPaint.getFontSpacing() * direction * ratio, viewPaint);
+            }
+
+            x_index += strWidths[i];
+
+        }
+
+//        canvas.restore();
+
 
     }
 
@@ -97,8 +168,9 @@ public class NumberSwitchView extends View {
         List<Animator> animators = new ArrayList<>();
 
         ObjectAnimator moveAnim = ObjectAnimator.ofFloat(
-                this, "ratio", 0f, 1f);
-        moveAnim.setRepeatCount(ValueAnimator.INFINITE);
+                this, "ratio", 1f, 0f);
+        moveAnim.setRepeatCount(-1);
+//        moveAnim.setRepeatMode(Animation.RESTART);
         moveAnim.setDuration(1000);
         moveAnim.setInterpolator(new LinearInterpolator());
         animators.add(moveAnim);
